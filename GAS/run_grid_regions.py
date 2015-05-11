2599226204a85424d104a005dc4372bb5f9c5e83
Jaime Pineda <jaime.e.pineda@gmail.com>
2015-05-11 18:46:53 +0200

    Add more regions in gridding
    -Added function to image all regions observed so far: OrionA, OrionB,
    NGC1333, L1455, B18, L1688

--- GAS/run_grid_regions.py
+++ GAS/run_grid_regions.py
@@ -3,15 +3,101 @@ from . import gridregion
 
 #from astropy.io import fits
 
-
 def grid_SerAqu():
-	gridregion.griddata( rootdir='/lustre/pipeline/scratch/GAS', region='Serpens_Aquila',dirname='Serpens_Aquila_NH3_11')
+    print("You will image the GBT Ammonia Survey data for Serpens_Aquila")
+    data_dir='/lustre/pipeline/scratch/GAS'
+    region_name='Serpens_Aquila'
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_11')
+    
+    hd_temp=fits.getheader(region_name'_NH3_11.fits')
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_22',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_33',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_C2S',        templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC5N',       templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_21_20', templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_22_21', templateHeader=hd_temp)
 
-	hd_temp=fits.getheader('Serpens_Aquila_NH3_11.fits')
-	gridregion.griddata( rootdir='/lustre/pipeline/scratch/GAS', region='Serpens_Aquila',dirname='Serpens_Aquila_NH3_22', templateHeader=hd_temp)
-	gridregion.griddata( rootdir='/lustre/pipeline/scratch/GAS', region='Serpens_Aquila',dirname='Serpens_Aquila_NH3_33', templateHeader=hd_temp)
-	gridregion.griddata( rootdir='/lustre/pipeline/scratch/GAS', region='Serpens_Aquila',dirname='Serpens_Aquila_C2S', templateHeader=hd_temp)
-	gridregion.griddata( rootdir='/lustre/pipeline/scratch/GAS', region='Serpens_Aquila',dirname='Serpens_Aquila_HC5N', templateHeader=hd_temp)
-	gridregion.griddata( rootdir='/lustre/pipeline/scratch/GAS', region='Serpens_Aquila',dirname='Serpens_Aquila_HC7N_21_20', templateHeader=hd_temp)
-	gridregion.griddata( rootdir='/lustre/pipeline/scratch/GAS', region='Serpens_Aquila',dirname='Serpens_Aquila_HC7N_22_21', templateHeader=hd_temp)
+def grid_NGC1333():
+    print("You will image the GBT Ammonia Survey data for NGC1333")
+    data_dir='/lustre/pipeline/scratch/GAS'
+    region_name='NGC1333'
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_11')
+    
+    hd_temp=fits.getheader(region_name'_NH3_11.fits')
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_22',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_33',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_C2S',        templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC5N',       templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_21_20', templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_22_21', templateHeader=hd_temp)
+
+def grid_L1455():
+    print("You will image the GBT Ammonia Survey data for L1455")
+    data_dir='/lustre/pipeline/scratch/GAS'
+    region_name='L1455'
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_11')
+    
+    hd_temp=fits.getheader(region_name'_NH3_11.fits')
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_22',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_33',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_C2S',        templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC5N',       templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_21_20', templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_22_21', templateHeader=hd_temp)
+
+def grid_L1688():
+    print("You will image the GBT Ammonia Survey data for L1688")
+    data_dir='/lustre/pipeline/scratch/GAS'
+    region_name='L1688'
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_11')
+    
+    hd_temp=fits.getheader(region_name'_NH3_11.fits')
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_22',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_33',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_C2S',        templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC5N',       templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_21_20', templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_22_21', templateHeader=hd_temp)
+
+def grid_OrionA():
+    print("You will image the GBT Ammonia Survey data for OrionA")
+    data_dir='/lustre/pipeline/scratch/GAS'
+    region_name='OrionA'
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_11')
+    
+    hd_temp=fits.getheader(region_name'_NH3_11.fits')
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_22',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_33',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_C2S',        templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC5N',       templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_21_20', templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_22_21', templateHeader=hd_temp)
+
+def grid_OrionB():
+    print("You will image the GBT Ammonia Survey data for OrionB")
+    data_dir='/lustre/pipeline/scratch/GAS'
+    region_name='OrionB'
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_11')
+    
+    hd_temp=fits.getheader(region_name'_NH3_11.fits')
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_22',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_33',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_C2S',        templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC5N',       templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_21_20', templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_22_21', templateHeader=hd_temp)
+
+def grid_B18():
+    print("You will image the GBT Ammonia Survey data for B18")
+    data_dir='/lustre/pipeline/scratch/GAS'
+    region_name='B18'
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_11')
+    
+    hd_temp=fits.getheader(region_name'_NH3_11.fits')
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_22',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_NH3_33',     templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_C2S',        templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC5N',       templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_21_20', templateHeader=hd_temp)
+    gridregion.griddata( rootdir=data_dir, region=region_name, dirname=region_name+'_HC7N_22_21', templateHeader=hd_temp)
 
