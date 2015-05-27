2a1f2110b0dda90511dffe2993a81fe23382b817
Katherine Lee <ijklee9@gmail.com>
2015-05-27 19:56:56 -0400

    L1455 added by Katherine L.

--- GAS/run_first_look.py
+++ GAS/run_first_look.py
@@ -201,3 +201,74 @@ def FirstLook_L1544():
     file_out=file_in.replace('.fits','_base1.fits')
     file_new=first_look.baseline( file_in, file_out, index_clean=index_rms, polyorder=1)
     first_look.peak_rms( file_new, index_rms=index_rms, index_peak=index_peak)
+
+def FirstLook_L1455():
+    print("Now NH3(1,1)")
+    a_rms = [   0, 140, 300, 410, 520, 680]
+    b_rms = [ 105, 270, 370, 480, 630, 750]
+    index_rms=first_look.create_index( a_rms, b_rms)
+    index_peak=np.arange(350,430)
+    file_in='L1455/L1455_NH3_11.fits'
+    file_out=file_in.replace('.fits','_base1.fits')
+    file_new=first_look.baseline( file_in, file_out, index_clean=index_rms, polyorder=1)
+    first_look.peak_rms( file_new, index_rms=index_rms, index_peak=index_peak)
+
+    print("Now NH3(2,2)")
+    a_rms = [   0, 340]
+    b_rms = [ 290, 650]
+    index_rms=first_look.create_index( a_rms, b_rms)
+    index_peak=np.arange(260,400)
+    file_in='L1455/L1455_NH3_22.fits'
+    file_out=file_in.replace('.fits','_base1.fits')
+    file_new=first_look.baseline( file_in, file_out, index_clean=index_rms, polyorder=1)
+    first_look.peak_rms( file_new, index_rms=index_rms, index_peak=index_peak)
+
+    print("Now NH3(3,3)")
+    a_rms = [   0, 340]  # No lines. Using the same as NH3(2,2)
+    b_rms = [ 290, 650]  # No lines. Using the same as NH3(2,2)
+    index_rms=first_look.create_index( a_rms, b_rms)
+    index_peak=np.arange(260,400)  # No lines. Using the same as NH3(2,2)
+    file_in='L1455/L1455_NH3_33.fits'
+    file_out=file_in.replace('.fits','_base1.fits')
+    file_new=first_look.baseline( file_in, file_out, index_clean=index_rms, polyorder=1)
+    first_look.peak_rms( file_new, index_rms=index_rms, index_peak=index_peak)
+
+    print("Now CCS")
+    a_rms = [   0, 350]  
+    b_rms = [ 290, 650]
+    index_rms=first_look.create_index( a_rms, b_rms)
+    index_peak=np.arange(260,390)  
+    file_in='L1455/L1455_C2S.fits'
+    file_out=file_in.replace('.fits','_base1.fits')
+    file_new=first_look.baseline( file_in, file_out, index_clean=index_rms, polyorder=1)
+    first_look.peak_rms( file_new, index_rms=index_rms, index_peak=index_peak)
+
+    print("Now HC5N")
+    a_rms = [   0, 350]  
+    b_rms = [ 290, 650]
+    index_rms=first_look.create_index( a_rms, b_rms)
+    index_peak=np.arange(260,390)  
+    file_in='L1455/L1455_HC5N.fits'
+    file_out=file_in.replace('.fits','_base1.fits')
+    file_new=first_look.baseline( file_in, file_out, index_clean=index_rms, polyorder=1)
+    first_look.peak_rms( file_new, index_rms=index_rms, index_peak=index_peak)
+
+    print("Now HC7N_21_20")
+    a_rms = [   0, 180]  
+    b_rms = [ 130, 298]
+    index_rms=first_look.create_index( a_rms, b_rms)
+    index_peak=np.arange(120,190)  
+    file_in='L1455/L1455_HC7N_21_20.fits'
+    file_out=file_in.replace('.fits','_base1.fits')
+    file_new=first_look.baseline( file_in, file_out, index_clean=index_rms, polyorder=1)
+    first_look.peak_rms( file_new, index_rms=index_rms, index_peak=index_peak)
+
+    print("Now HC7N_22_21")
+    a_rms = [   0, 180]  # No lines. Using the same as HC7N_21_20
+    b_rms = [ 130, 298]  # No lines. Using the same as HC7N_21_20
+    index_rms=first_look.create_index( a_rms, b_rms)
+    index_peak=np.arange(120,190)  # No lines. Using the same as HC7N_21_20
+    file_in='L1455/L1455_HC7N_22_21.fits'
+    file_out=file_in.replace('.fits','_base1.fits')
+    file_new=first_look.baseline( file_in, file_out, index_clean=index_rms, polyorder=1)
+    first_look.peak_rms( file_new, index_rms=index_rms, index_peak=index_peak)
