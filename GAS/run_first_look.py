fe0559d4697b6fe5fa55566f784e41a8e3fa368a
Erik Rosolowsky <rosolowsky@ualberta.ca>
2016-02-11 09:40:32 -0700

    Template First look function

--- GAS/run_first_look.py
+++ GAS/run_first_look.py
@@ -13,6 +13,7 @@ quit_message=textwrap.dedent("""\
     processed in this release or it is not yet implemented.""")
 
 def GenerateRegions():
+
 #    gasPipeline.updateLogs()
 #    gasPipeline.updateCatalog()
     obs = Table.read('ObservationLog.csv')
@@ -20,7 +21,6 @@ def GenerateRegions():
 
 # This takes out rows that are empty
 # This needs to be done twice for some reason... 
-
     for idx, row in enumerate(cat):
         if not row['BoxName']:
             cat.remove_row(idx)
@@ -28,21 +28,51 @@ def GenerateRegions():
     for idx, row in enumerate(cat):
         if not row['BoxName']:
             cat.remove_row(idx)
-    jt = join(obs,cat,keys='BoxName')
     obs.rename_column('Source','BoxName')
     joincat = join(obs,cat,keys='BoxName')
-
-    import pdb; pdb.set_trace()
+    groupcat = joincat.group_by('Region name')
+    mean_values = groupcat.groups.aggregate(np.mean)
+    return(mean_values)
             
 def FirstLook(regions=None, file_extension='_all',
               region_parameters='region_parameters.csv'):
-    
-    if regions is None:
 
-        linelist = ['NH3_11','NH3_22','NH3_33','C2S','HC5N','HC7N_21_20','HC7N_22_21']
-        region = ['B1','B18','B1E','B59','Cepheus','HC2','IC348','IC5146','L1448',
-                  'L1451','L1455','L1688','L1689','NGC1333','OrionA','OrionB',
-                  'OrionB_NGC2023-2024','OrionB_NGC2068-2071', 'Serpens_Aquila','TMC1']
+    RegionCatalog = GenerateRegions()
+    for ThisRegion in RegionCatalog:
+        region_name=ThisRegion['Region name']
+        print("Now NH3(1,1)")
+        a_rms = [  0, 150, 310, 420, 530, 690]
+        b_rms = [ 60, 230, 330, 440, 610, 760]
+        index_rms=first_look.create_index( a_rms, b_rms)
+        index_peak=np.arange(340,420)
+        file_in='{0}/{0}_NH3_11{1}.fits'.format(region_name,file_extension)
+        file_out=file_in.replace(file_extension+'.fits',
+                                 '_base'+file_extension+'.fits')
+        first_look.baseline( file_in, file_out, index_clean=index_rms, polyorder=1)
+        first_look.peak_rms( file_out, index_rms=index_rms, index_peak=index_peak)
+    # 
+        linelist = ['NH3_22','NH3_33','C2S','HC5N','HC7N_21_20','HC7N_22_21']
+        vsys = ThisRegion['VLSR']*u.km/u.s
+        throw = 5*u.km/u.s
+        for line in linelist:
+            file_in = '{0}/{0}_{1}{2}.fits'.format(region_name,line,file_extension)
+            s = SpectralCube.read(file_in)
+            s = s.with_spectral_unit(u.km/u.s,velocity_convention='radio')
+            a_rms = [s.closest_spectral_channel(vsys+2*throw),
+                     s.closest_spectral_channel(vsys-throw)]
+            b_rms = [s.closest_spectral_channel(vsys+throw),
+                     s.closest_spectral_channel(vsys-2*throw)]
+            index_peak = np.arange(s.closest_spectral_channel(vsys+3*u.km/u.s),
+                                   s.closest_spectral_channel(vsys-3*u.km/u.s))
+            index_rms=first_look.create_index( a_rms, b_rms)
+            
+            file_out=file_in.replace(file_extension+'.fits',
+                                     '_base'+file_extension+'.fits')
+            first_look.baseline( file_in, file_out, 
+                                 index_clean=index_rms, polyorder=1)
+            first_look.peak_rms( file_out, index_rms=index_rms, 
+                                 index_peak=index_peak)
+    
 
     
 
