7d48e474cc3cec1cf04ab499426c85b258af28de
Jaime Pineda <jaime.e.pineda@gmail.com>
2016-05-11 18:23:08 +0200

    Update line fitting and first look documentation
    - Updated call to cubefit and plot_cubefit to include the file_extension keywords

--- GAS/PropertyMaps.py
+++ GAS/PropertyMaps.py
@@ -100,29 +100,29 @@ def run_plot_fit_all():
     results for all regions
     """
     cubefit(region='L1455', blorder=1, do_plot=True, snr_min=3, multicore=1, 
-            vmax=6.7, vmin=3.5)
+            vmax=6.7, vmin=3.5, file_extension='base_DR1')
     plot_cubefit(region='L1455', distance=250*u.pc, dvmin=0.05, dvmax=0.45, 
-                 vcmin=4.5, vcmax=6.0)
+                 vcmin=4.5, vcmax=6.0, file_extension='base_DR1')
 
     cubefit(region='NGC1333', blorder=1, do_plot=True, snr_min=3, multicore=1,
-            vmax=9.5, vmin=4.2)
+            vmax=9.5, vmin=4.2, file_extension='base_DR1')
     plot_cubefit(region='NGC1333', distance=250*u.pc, dvmin=0.05, dvmax=0.6, 
-                 vcmin=6.4, vcmax=9.3)
+                 vcmin=6.4, vcmax=9.3, file_extension='base_DR1')
 
     cubefit(region='B18', vmin=4.5, vmax=7.5, do_plot=False, snr_min=3.0, 
-            multicore=1)
+            multicore=1, file_extension='base_DR1')
     plot_cubefit(region='B18', distance=137*u.pc, dvmin=0.05, dvmax=0.3, 
-                 vcmin=5.7, vcmax=6.7)
+                 vcmin=5.7, vcmax=6.7, file_extension='base_DR1')
 
     cubefit(region='L1688', vmin=2.5, vmax=5.5, do_plot=False, snr_min=3.0, 
-            multicore=1)
+            multicore=1, file_extension='base_DR1')
     plot_cubefit(region='L1688', distance=120*u.pc, dvmin=0.05, dvmax=0.7, 
-                 vcmin=2.7, vcmax=4.8)
+                 vcmin=2.7, vcmax=4.8, file_extension='base_DR1')
 
     cubefit(region='OrionA', vmin=5.6, vmax=13.7, do_plot=False, snr_min=3.0, 
-            multicore=1)
+            multicore=1, file_extension='base_DR1')
     plot_cubefit(region='OrionA', distance=450*u.pc, dvmin=0.05, dvmax=0.7, 
-                 vcmin=5.7, vcmax=12.7)
+                 vcmin=5.7, vcmax=12.7, file_extension='base_DR1')
 
 
 
