f3f04d9439c4c75cfb9155c49197ebf92f3c58e1
Erik Rosolowsky <rosolowsky@ualberta.ca>
2016-06-23 03:35:14 -0600

    Your code will be easy to read, they said

--- GAS/utils.py
+++ GAS/utils.py
@@ -1,14 +1,12 @@
-import astropy.coordinates as coords
 import catalogs
-
 import numpy as np
 
 
-def VlsrByCoord(RA, Dec, region = 'OrionA', regionCatalog = None):
+def VlsrByCoord(RA, Dec, region='OrionA', regionCatalog=None):
 
     """
-    For the GAS regions determine the appropriate v0 given coordinates.  
-    If a velocity gradient has been fit, it is hard-coded here.  Otherwise, 
+    For the GAS regions determine the appropriate v0 given coordinates.
+    If a velocity gradient has been fit, it is hard-coded here.  Otherwise,
     this uses the catalog velocities.
 
     Parameters
@@ -20,42 +18,43 @@ def VlsrByCoord(RA, Dec, region = 'OrionA', regionCatalog = None):
     Region : str
         The GAS name of the region, used in catalog lookups.
     regionCatalog : astropy.table.Table
-        Table containing the catalog values of the velocity to prevent 
+        Table containing the catalog values of the velocity to prevent
         repeated catalog parsing
 
     Returns
-    ------- 
+    -------
     v0 : float
-        Estimate of central velocity of emission spectrum in km/s 
+        Estimate of central velocity of emission spectrum in km/s
     """
 
     if 'NGC1333' in region:
         v0 = 7.6687698945600005\
-            -0.10015533*(RA-52.2275368656)\
-            +1.29725507*(Dec-31.2380245486)
+            - 0.10015533 * (RA - 52.2275368656)\
+            + 1.29725507 * (Dec - 31.2380245486)
         return(v0)
 
     if 'OrionA' in region:
-        coeffs = [-2.8256074 , -4.65791997,  9.14502305]
-        v0 = coeffs[2]+\
-            coeffs[0]*(RA-83.446122802665869)+\
-            coeffs[1]*(Dec+6.0050560818354661)
+        coeffs = [-2.8256074, -4.65791997, 9.14502305]
+        v0 = coeffs[2] + \
+            coeffs[0] * (RA - 83.446122802665869) + \
+            coeffs[1] * (Dec + 6.0050560818354661)
         return(v0)
 
     if regionCatalog is None:
         regionCatalog = catalogs.GenerateRegions()
 
     try:
-        v0 = regionCatalog[regionCatalog['Region name']==
+        v0 = regionCatalog[regionCatalog['Region name'] ==
                            region]['VLSR'].data.data[0]
         return v0
     except IndexError:
         return np.nan
 
+
 def FitGradient(vlsrData, vlsrWCS):
 
     """
-    Utility function for fitting a velocity gradient.  Gives coefficients to 
+    Utility function for fitting a velocity gradient.  Gives coefficients to
     hardwire into the VlsrByCoord function.
 
     Parameters
@@ -64,33 +63,33 @@ def FitGradient(vlsrData, vlsrWCS):
         Image of the vlsr data
     vlsrWCS : astropy.wcs.WCS
         WCS object corresponding to the image
-    
+
     Returns
     -------
     None -- output print to screen.
     """
 
-    ygood,xgood = np.where((vlsrData!=0)*np.isfinite(vlsrData))
-    ra, dec = vlsrWCS.wcs_pix2world(xgood,ygood,0)
-    rabar= np.median(ra)
+    ygood, xgood = np.where((vlsrData != 0) * np.isfinite(vlsrData))
+    ra, dec = vlsrWCS.wcs_pix2world(xgood, ygood, 0)
+    rabar = np.median(ra)
     decbar = np.median(dec)
-    vbar = np.median(vlsrData[ygood,xgood])
-    vvals = vlsrData[ygood,xgood]-vbar
+    vbar = np.median(vlsrData[ygood, xgood])
+    vvals = vlsrData[ygood, xgood] - vbar
 
-    A = np.r_[[np.ones(len(ra))],[ra-rabar],[dec-decbar]]
-    invcov = np.linalg.inv(np.matrix(A)*np.matrix(A.T))
-    
+    A = np.r_[[np.ones(len(ra))], [ra - rabar], [dec - decbar]]
+    invcov = np.linalg.inv(np.matrix(A) * np.matrix(A.T))
+
     w = np.matrix([np.sum(vvals),
-                   np.sum(vvals*(ra-rabar)),
-                   np.sum(vvals*(dec-decbar))]).T
-    coeffs = np.array(invcov*w)
+                   np.sum(vvals * (ra - rabar)),
+                   np.sum(vvals * (dec - decbar))]).T
+    coeffs = np.array(invcov * w)
 
     stringarr = ['Gradient Values',
                  'meanRA  = {0}'.format(rabar),
                  'meanDec = {0}'.format(decbar),
                  'gradRA  = {0}'.format(coeffs[1]),
                  'gradDec = {0}'.format(coeffs[2]),
-                 'voffset = {0}'.format(coeffs[0]+vbar)]
+                 'voffset = {0}'.format(coeffs[0] + vbar)]
     for i in stringarr:
         print(i)
 
