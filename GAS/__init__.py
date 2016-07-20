2818336d67ce7e2a1c00a2e2d33e718d677b7378
Jaime Pineda <jaime.e.pineda@gmail.com>
2016-07-20 19:14:23 +0200

    Update __init__.py

--- GAS/__init__.py
+++ GAS/__init__.py
@@ -17,8 +17,8 @@ if not _ASTROPY_SETUP_:
     import gasPipeline
 #    import gridregion
 #    import run_grid_regions
-    import first_look
-    import run_first_look
+#    import first_look
+#    import run_first_look
 #    import PropertyMaps
     import gasBinning
     import voronoi_2d_binning
