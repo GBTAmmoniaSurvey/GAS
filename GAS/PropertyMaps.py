5d782f79c636a80db9630a6be2f8d2d969afbfd9
rfriesen <friesen.rae@gmail.com>
2016-08-02 10:22:39 -0400

    Update flag property maps

--- GAS/PropertyMaps.py
+++ GAS/PropertyMaps.py
@@ -183,19 +183,19 @@ def flag_all_data(region='OrionA',blorder='1',rmsLim=0.2, file_extension=None):
     else:
         root = 'base{0}'.format(blorder)
 
-    hdu=fits.open("{0}_parameter_maps_{1}.fits".format(region,root))
+    hdu=fits.open("{0}/{0}_parameter_maps_{1}.fits".format(region,root))
     hd=hdu[0].header
     cube=hdu[0].data
     hdu.close()
 
-    rms11hdu = fits.open("{0}/{0}_NH3_11_base{1}_mom0_sigma_QA.fits.fits".format(region,blorder))
+    rms11hdu = fits.open("{0}/{0}_NH3_11_base{1}_mom0_sigma.fits".format(region,blorder))
     rms11data = rms11hdu[0].data
     rms11hdu.close()
     m0_11 = fits.open("{0}/{0}_NH3_11_base{1}_mom0.fits".format(region,blorder))
     m0_11data = m0_11[0].data
     hd11 = m0_11[0].header
     m0_11.close()
-    rms22hdu = fits.open("{0}/{0}_NH3_22_base{1}_mom0_sigma_QA.fits".format(region,blorder))
+    rms22hdu = fits.open("{0}/{0}_NH3_22_base{1}_mom0_sigma.fits".format(region,blorder))
     rms22data = rms22hdu[0].data
     rms22hdu.close()
     m0_22 = fits.open("{0}/{0}_NH3_22_base{1}_mom0.fits".format(region,blorder))
