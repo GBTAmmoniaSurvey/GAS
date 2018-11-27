73e8817e28e60ad7a4dedbb29f367ed609aafb1c
Rachel Friesen <friesen.rae@gmail.com>
2018-11-27 13:53:17 -0500

    Updating config file for moments

--- GAS/config.py
+++ GAS/config.py
@@ -6,6 +6,16 @@ L1688 = {'scalebar_size': 0.1*u.pc, 'scalebar_pos': 'bottom right',
          'sigma_mult' : 1.
          }
 
+L1689 = {'scalebar_size': 0.1*u.pc, 'scalebar_pos': 'bottom right',
+         'distance' : 137.3*u.pc,
+         'sigma_mult' : 1.
+         }
+
+L1712 = {'scalebar_size': 0.1*u.pc, 'scalebar_pos': 'bottom right',
+         'distance' : 137.3*u.pc,
+         'sigma_mult' : 1.
+         }
+
 B18 = {'scalebar_size': 0.1*u.pc, 'scalebar_pos': 'bottom right',
        'distance' : 135*u.pc,
        'sigma_mult' : 1.
@@ -27,6 +37,21 @@ OrionA_S = {'scalebar_size': 0.5*u.pc, 'scalebar_pos': 'bottom right',
          }
 
 B1 = {'scalebar_size': 0.1*u.pc, 'scalebar_pos': 'bottom right',
+      'distance' : 250.*u.pc,
+      'sigma_mult' : 1.
+         }
+
+L1451 = {'scalebar_size': 0.1*u.pc, 'scalebar_pos': 'bottom right',
+      'distance' : 250.*u.pc,
+      'sigma_mult' : 1.
+         }
+
+L1448 = {'scalebar_size': 0.1*u.pc, 'scalebar_pos': 'bottom right',
+      'distance' : 250.*u.pc,
+      'sigma_mult' : 1.
+         }
+
+L1455 = {'scalebar_size': 0.1*u.pc, 'scalebar_pos': 'bottom right',
       'distance' : 250.*u.pc,
       'sigma_mult' : 1.
          }
@@ -96,7 +121,9 @@ Serpens_Aquila = {'scalebar_size': 0.1*u.pc, 'scalebar_pos': 'bottom right',
 
 plottingDictionary = {"L1688" : L1688, "B18" : B18, "NGC1333" : NGC1333, 
                       "OrionA" : OrionA, "OrionA_S" : OrionA_S, "B1" : B1, 
+                      "L1448" : L1448, "L1451" : L1451, "L1455" : L1455,
                       "HC2" : HC2, "Pipe_Core40" : Pipe_Core40, "Perseus" : Perseus,
+                      "L1689" : L1689, "L1712" : L1712, 
                       "OrionB_NGC2068-2071" : OrionB_NGC2068_2071, 
                       "OrionB_NGC2023-2024" : OrionB_NGC2023_2024,
                       "IC5146" : IC5146, "IC348" : IC348, "Cepheus_L1228" : Cepheus_L1228, 
